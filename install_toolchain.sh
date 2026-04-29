6400579d3700f70ec9fc33587be1349e5290e5a2
bat52 <marcomerli+github@gmail.com>
2026-04-30 00:11:57 +0200

    add verilator to install_toolchain.sh

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -3,3 +3,4 @@ sudo apt update
 sudo apt install gcc-riscv64-linux-gnu g++-riscv64-linux-gnu \
                  binutils-riscv64-linux-gnu \
                  gcc-riscv64-unknown-elf
+sudo apt install verilator
